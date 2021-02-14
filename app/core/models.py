8b0d7d18f163ba775ec7b090eda6c67ab84fa3c0
souhil433 <souhil433@gmail.com>
2021-02-14 10:51:09 +0100

    Added create tag feature.

--- app/core/models.py
+++ app/core/models.py
@@ -46,8 +46,10 @@ class Tag(models.Model):
     """Tag to be used for a recipe"""
 
     name = models.CharField(max_length=255)
+
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
     )
 
     def __str__(self):
